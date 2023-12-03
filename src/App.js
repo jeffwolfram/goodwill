913bbb041caa6f070cb087904367aaed0200178a
Jeff Wolfram <jeffwolfram@gmail.com>
2023-12-02 16:14:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const [buttonData, setButtonData] = useState([
+    { name: 'Speakers', broken: 0, ready: 0 },
+    { name: 'Soundbar', broken: 0, ready: 0 },
+    { name: 'TV', broken: 0, ready: 0 },
+    { name: 'Computer Monitor', broken: 0, ready: 0 },
+    { name: 'Game Console', broken: 0, ready: 0 },
+    { name: 'Video Game', broken: 0, ready: 0 },
+    { name: 'AV Player', broken: 0, ready: 0 },
+    { name: 'IOT', broken: 0, ready: 0 },
+  ]);
+
+  const [totalBroken, setTotalBroken] = useState(0);
+  const [totalReady, setTotalReady] = useState(0);
+
+  const [isConfirmationOpen, setConfirmationOpen] = useState(false);
+
+  const openConfirmationDialog = () => {
+    setConfirmationOpen(true);
+  };
+
+  const closeConfirmationDialog = () => {
+    setConfirmationOpen(false);
+  };
+
+  const handleButtonClick = (index, type) => {
+    const updatedData = [...buttonData];
+    updatedData[index][type] += 1;
+    setButtonData(updatedData);
+    if (type === 'broken') {
+      setTotalBroken((prevTotal) => prevTotal + 1)
+    } else if (type === 'ready') {
+      setTotalReady((prevTotal) => prevTotal + 1);
+    }
+  };
+
+  const handleResetButtonClick = () => {
+    openConfirmationDialog(); 
+  };
+
+  const handleReset = () => {
+    const resetData = buttonData.map((button) => ({ ...button, broken: 0, ready: 0 }));
+    setButtonData(resetData);
+    setTotalBroken(0);
+    setTotalReady(0);
+    closeConfirmationDialog(); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="button-container">
+        <h2>Buttons:</h2>
+        {buttonData.map((button, index) => (
+          <div key={index} className="item-container">
+            <span className="item-name">{button.name}</span>
+            <button
+              className="custom-button"
+              onClick={() => handleButtonClick(index, 'broken')}
+            >
+              Broken ({button.broken})
+            </button>
+            <button
+              className="custom-button"
+              onClick={() => handleButtonClick(index, 'ready')}
+            >
+              Ready ({button.ready})
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="result-container">
+        <h2>Results:</h2>
+        <div className="result-list">
+          {buttonData.map((button, index) => (
+            <div key={index} className="result-item">
+              <span>
+                {button.name} - Broken: {button.broken}, Ready: {button.ready}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div className="table">
+          <table>
+            <tr>
+              <th>Broken</th>
+              <th>Ready</th>
+            </tr>
+            <tr>
+              <td>{totalBroken}</td>
+              <td>{totalReady}</td>
+            </tr>
+          </table>
+          
+        </div>
+
+        <button onClick={handleResetButtonClick}>Reset All</button>
+        {isConfirmationOpen && (
+          <div className="confirmation-dialog">
+            <p>Are you sure you want to reset all counts?</p>
+            <button onClick={handleReset}>Yes</button>
+            <button onClick={closeConfirmationDialog}>No</button>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
